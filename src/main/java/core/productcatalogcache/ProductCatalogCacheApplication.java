cdb1d5e961847a7655062c4349da1df76a45ece8
Pasha Fentisov <pasha16.ua@gmail.com>
2024-11-25 14:44:59 +0200

    Initial commit

---
+++ src/main/java/core/productcatalogcache/ProductCatalogCacheApplication.java
@@ -0,0 +1,13 @@
+package core.productcatalogcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/core/productcatalogcache/ProductCatalogCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package core.productcatalogcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
